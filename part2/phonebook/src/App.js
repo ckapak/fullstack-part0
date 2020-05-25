63263cd87a1bb222b412165a3d5ad77731e93398
ckapak <christinekapak@googlemail.com>
2020-05-25 09:06:40 +0100

    revised and amended upto 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,28 +2,32 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([{
-    name: 'Arto Hellas', id: 1
+    name: 'Arto Hellas'
   }])
   const [newName, setNewName] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
-    // console.log('button clicked', e.target)
+
+    const filterDuplicateName = persons.filter(
+      person => person.name === newName
+    )
+
+    if (filterDuplicateName.length > 0) {
+      alert(`${newName} is already added to phonebook`)
+      setNewName("")
+      return
+    }
+
     const personObject = {
       name: newName,
-      id: persons.length + 1,
     }
     setPersons(persons.concat(personObject))
     setNewName('')
   }
 
-  const handlePersonChange = (e) => {
-    // console.log(e.target.value)
+  const handleNewName = (e) => {
     setNewName(e.target.value)
-  }
-
-  if (persons.some(e => e.name === newName)) {
-    alert(`${newName} is already added to phonebook.`)
   }
 
   return (
@@ -33,16 +37,15 @@ const App = () => {
         <div>
           name: <input
             value={newName}
-            onChange={handlePersonChange} />
+            onChange={handleNewName} />
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
-          <div key={person.id} >
-            {person.name} </ div>
-        )}
+        {persons.map((person, i) => (
+          <li key={i}>{person.name}</li>
+        ))}
       </ul>
     </div>
   )
